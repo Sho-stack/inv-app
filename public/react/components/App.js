eb977330127cc68712220afe82576cd0e575acfd
michalszostak <michal.szostak56@gmail.com>
2023-03-06 17:07:46 +0000

    bootstrap nav-bar added

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,32 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { PagesList } from './PagesList';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
 
 export const App = () => {
 
-	const [pages, setPages] = useState([]);
-
-	async function fetchPages(){
-		try {
-			const response = await fetch(`${apiURL}/wiki`);
-			const pagesData = await response.json();
-			setPages(pagesData);
-		} catch (err) {
-			console.log("Oh no an error! ", err)
-		}
-	}
-
-	useEffect(() => {
-		fetchPages();
-	}, []);
-
-	return (
+	return (<>
+		<Navbar bg="dark" variant="dark">
+			<Container>
+			<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+			<Nav className="me-auto">
+				<Nav.Link href="#home">Home</Nav.Link>
+				<Nav.Link href="#features">Features</Nav.Link>
+				<Nav.Link href="#pricing">Pricing</Nav.Link>
+			</Nav>
+			</Container>
+		</Navbar>
 		<main>	
-      <h1>WikiVerse</h1>
-			<h2>An interesting 📚</h2>
-			<PagesList pages={pages} />
+
 		</main>
-	)
-}
+	</>)}
